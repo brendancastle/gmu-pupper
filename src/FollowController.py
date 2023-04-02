9ed2b5a8e25271e3cd7c2ce6b071a7d3ec2e478b
Saad Muhammad Abdul Ghani <saadhku2@gmail.com>
2023-04-02 12:10:07 -0400

    trotting in following

--- src/FollowController.py
+++ src/FollowController.py
@@ -33,7 +33,6 @@ class FollowController(Controller):
         if command.follow_event:
             if self.in_follow_state == False:
                 # rest until start_following_event
-                state.behavior_state = self.stand_transition_mapping[state.behavior_state]
                 self.in_follow_state = True
                 command.stand_event = True
                 super().run(state, command)
@@ -43,12 +42,12 @@ class FollowController(Controller):
                 super().run(state, command)
                 print("t pressed, in_follow_state exited")
         elif self.in_follow_state:
+
             if command.start_stop_following_event and self.following == False:
                 self.following = True
                 self.count = 0
                 print("u pressed, following started")
             elif command.start_stop_following_event and self.following == True:
-                state.behavior_state = self.stand_transition_mapping[state.behavior_state]
                 command.stand_event = True
                 self.following = False
                 super().run(state, command)
@@ -64,37 +63,42 @@ class FollowController(Controller):
                 # calculate commands based on depth, direction
                 # pass to super
                 
-                self.eps = 1
-                self.slow_down_distance = 2
+                # self.eps = 1
+                # self.slow_down_distance = 2
                 
-                def calc_how_far(d):
-                    if d > self.slow_down_distance:
-                        return 1
-                    if d < self.eps:
-                        return 0
-                    return (d-self.eps)/(self.slow_down_distance-self.eps)
-                how_far = calc_how_far(depth)
+                # def calc_how_far(d):
+                #     if d > self.slow_down_distance:
+                #         return 1
+                #     if d < self.eps:
+                #         return 0
+                #     return (d-self.eps)/(self.slow_down_distance-self.eps)
+                # how_far = calc_how_far(depth)
 
-                l_alpha = 0.15
-                # lets just move right and straight
-                self.lx_ = l_alpha * 1 + (1 - l_alpha) * self.lx_ 
+                # l_alpha = 0.15
+                # # lets just move right and straight
+                # self.lx_ = l_alpha * 1 + (1 - l_alpha) * self.lx_ 
 
-                # self.ly_ = l_alpha * 1 + (1 - l_alpha) * self.ly_ 
-                # self.msg["ly"] = self.ly_
+                # # self.ly_ = l_alpha * 1 + (1 - l_alpha) * self.ly_ 
+                # # self.msg["ly"] = self.ly_
 
-                # x_vel = msg["ly"] * self.config.max_x_velocity
-                y_vel = self.lx_ * -self.config.max_y_velocity*how_far
+                # # x_vel = msg["ly"] * self.config.max_x_velocity
+                # y_vel = self.lx_ * -self.config.max_y_velocity*how_far
+                
+                # if y_vel != 0:
+                #     command.horizontal_velocity = np.array([0, y_vel])
+                #     state.behavior_state = self.walk_transition_mapping[state.behavior_state]
+                #     super().run(state, command)
+                #     self.count += 1
+                # else:
+                #     print("goal reached!")
+                #     state.behavior_state = self.stand_transition_mapping[state.behavior_state]
+                #     command.stand_event = True
+                #     super().run(state, command)
 
-                if y_vel != 0:
-                    command.horizontal_velocity = np.array([0, y_vel])
-                    state.behavior_state = self.walk_transition_mapping[state.behavior_state]
-                    super().run(state, command)
-                    self.count += 1
-                else:
-                    print("goal reached!")
-                    state.behavior_state = self.stand_transition_mapping[state.behavior_state]
-                    command.stand_event = True
-                    super().run(state, command)
+                command.trot_event = True
+                print("following. should be trotting")
+                super().run(state,command)
+                
                     
                 
         else:
