ccf5fb17cd19591267e7515445a61a57dcbe9b39
Saad Muhammad Abdul Ghani <saadhku2@gmail.com>
2023-04-15 00:14:45 -0400

    draft followCtrl for getting camera depth, coord

--- get_camera.py
+++ get_camera.py
@@ -13,7 +13,7 @@ PYBULLET_KD_GAIN = 0.2
 
 class Camera_serial:
 
-    def __init__(self, cam_skip_frames, distance, show_camera=True):
+    def __init__(self, cam_skip_frames, distance, show_camera=True, return_true_output=False):
         self.cam_skip=0
         self.cam_skip_frames = cam_skip_frames
         self.distance = distance
@@ -54,82 +54,89 @@ class Camera_serial:
 
         self.prev_pos = None
 
+        self.return_true_output = return_true_output
+
     def get_camera_details(self):
+        if self.return_true_output:
 
-        if self.distance_calculated_from_pybullet:
-            max_distance = 10000
-            cone_id = None
-            for i in range (self.p.getNumBodies()):
-                b = self.p.getBodyUniqueId(i)
-                if self.p.getBodyInfo(b)[1]==b'cone':
-                    cone_id = b
-                    break
-            if cone_id is None:
-                return None, None
-            closest_points = self.p.getClosestPoints(self.pupper_body_uid, cone_id, max_distance)
-            
-            min_dist = max_distance
-            object_pos = None
-            for i in range(len(closest_points)):
-                if min_dist > closest_points[i][8]:
-                    min_dist = closest_points[i][8]
-                    object_pos = closest_points[i][6]
-            
-            if object_pos is not None:
-                self.prev_pos = object_pos
-            
-            if self.prev_pos is None:
-                return None, None
 
-            pos, orn = self.p.getBasePositionAndOrientation(self.pupper_body_uid)
-            yaw_0 = self.p.getEulerFromQuaternion(orn)[2]
-            
-            theta1 = math.atan2(self.prev_pos[1]-pos[1],self.prev_pos[0]-pos[0])
-            def limit_fn(x):
-                if x> 2*math.pi or x < -2*math.pi:
-                    raise Exception(f"limit_fn not defined for x>2pi or x<-2pi. value of x: {x:.4f}")
-                if -math.pi < x and x < math.pi:
-                    return x
-                elif math.pi < x:
-                    return -math.pi + (x%math.pi)
-                else:
-                    return math.pi + (x%-math.pi)
-            def wrap_around_fn(x):
-                if x> 2*math.pi or x < -2*math.pi:
-                    raise Exception(f"wrap_around_fn not defined for x>2pi or x<-2pi. value of x: {x:.4f}")
-                if -math.pi < x and x < math.pi:
-                    return x
-                elif math.pi < x:
-                    return -2*math.pi + x
-                else:
-                    return 2*math.pi + x
-
-            a_hat, b_hat = 0.99834, 1.5696
-            calculated_yaw = theta1*a_hat + b_hat
-            calculated_yaw = limit_fn(calculated_yaw)
-            delta_yaw = wrap_around_fn(calculated_yaw-yaw_0)
-            
-            return delta_yaw, min_dist
-        else:
-            if (self.cam_skip>self.cam_skip_frames):
-                agent_pos, agent_orn = self.p.getBasePositionAndOrientation(self.pupper_body_uid)
-
-                yaw = self.p.getEulerFromQuaternion(agent_orn)[0]
-                xA, yA, zA = agent_pos
-                zA = zA + 0.07
-                xA = xA +0.09
-
-                xB = xA + math.cos(yaw)*self.distance
-                yB = yA + math.sin(yaw)*self.distance
-                zB = zA 
-                
-                view_matrix = self.p.computeViewMatrix(cameraEyePosition=[xA,yA,zA], cameraTargetPosition=[xB,yB,zB], cameraUpVector=[0,0,1.0])
-
-                projection_matrix = self.p.computeProjectionMatrixFOV(fov=90, aspect=1.5, nearVal=0.02, farVal=3.5)
-                imgs = self.p.getCameraImage(self.img_w, self.img_h, view_matrix, projection_matrix, renderer=self.p.ER_BULLET_HARDWARE_OPENGL)
-                _,_, rgb, depth, segmentation = imgs
-                self.cam_skip = 0
-
-                return rgb, depth
-            self.cam_skip += 1
             return None, None
+        else:
+                
+            if self.distance_calculated_from_pybullet:
+                max_distance = 10000
+                cone_id = None
+                for i in range (self.p.getNumBodies()):
+                    b = self.p.getBodyUniqueId(i)
+                    if self.p.getBodyInfo(b)[1]==b'cone':
+                        cone_id = b
+                        break
+                if cone_id is None:
+                    return None, None
+                closest_points = self.p.getClosestPoints(self.pupper_body_uid, cone_id, max_distance)
+                
+                min_dist = max_distance
+                object_pos = None
+                for i in range(len(closest_points)):
+                    if min_dist > closest_points[i][8]:
+                        min_dist = closest_points[i][8]
+                        object_pos = closest_points[i][6]
+                
+                if object_pos is not None:
+                    self.prev_pos = object_pos
+                
+                if self.prev_pos is None:
+                    return None, None
+
+                pos, orn = self.p.getBasePositionAndOrientation(self.pupper_body_uid)
+                yaw_0 = self.p.getEulerFromQuaternion(orn)[2]
+                
+                theta1 = math.atan2(self.prev_pos[1]-pos[1],self.prev_pos[0]-pos[0])
+                def limit_fn(x):
+                    if x> 2*math.pi or x < -2*math.pi:
+                        raise Exception(f"limit_fn not defined for x>2pi or x<-2pi. value of x: {x:.4f}")
+                    if -math.pi < x and x < math.pi:
+                        return x
+                    elif math.pi < x:
+                        return -math.pi + (x%math.pi)
+                    else:
+                        return math.pi + (x%-math.pi)
+                def wrap_around_fn(x):
+                    if x> 2*math.pi or x < -2*math.pi:
+                        raise Exception(f"wrap_around_fn not defined for x>2pi or x<-2pi. value of x: {x:.4f}")
+                    if -math.pi < x and x < math.pi:
+                        return x
+                    elif math.pi < x:
+                        return -2*math.pi + x
+                    else:
+                        return 2*math.pi + x
+
+                a_hat, b_hat = 0.99834, 1.5696
+                calculated_yaw = theta1*a_hat + b_hat
+                calculated_yaw = limit_fn(calculated_yaw)
+                delta_yaw = wrap_around_fn(calculated_yaw-yaw_0)
+                
+                return delta_yaw, min_dist
+            else:
+                if (self.cam_skip>self.cam_skip_frames):
+                    agent_pos, agent_orn = self.p.getBasePositionAndOrientation(self.pupper_body_uid)
+
+                    yaw = self.p.getEulerFromQuaternion(agent_orn)[0]
+                    xA, yA, zA = agent_pos
+                    zA = zA + 0.07
+                    xA = xA +0.09
+
+                    xB = xA + math.cos(yaw)*self.distance
+                    yB = yA + math.sin(yaw)*self.distance
+                    zB = zA 
+                    
+                    view_matrix = self.p.computeViewMatrix(cameraEyePosition=[xA,yA,zA], cameraTargetPosition=[xB,yB,zB], cameraUpVector=[0,0,1.0])
+
+                    projection_matrix = self.p.computeProjectionMatrixFOV(fov=90, aspect=1.5, nearVal=0.02, farVal=3.5)
+                    imgs = self.p.getCameraImage(self.img_w, self.img_h, view_matrix, projection_matrix, renderer=self.p.ER_BULLET_HARDWARE_OPENGL)
+                    _,_, rgb, depth, segmentation = imgs
+                    self.cam_skip = 0
+
+                    return rgb, depth
+                self.cam_skip += 1
+                return None, None
---
+++ src/FollowController2.py
@@ -0,0 +1,101 @@
+from src.Controller import Controller
+from src.State import BehaviorState
+import numpy as np
+
+from get_camera import Camera_serial
+
+class FollowController(Controller):
+    def __init__(self, config, inverse_kinematics):
+        super().__init__(config, inverse_kinematics)
+
+        self.in_follow_state = False
+        self.following = False
+        self.rx_ = 0.0
+        self.ry_ = 0.0
+        self.lx_ = 0.0
+        self.ly_ = 0.0
+
+        self.l_alpha = 0.15
+        self.r_alpha = 0.1
+        
+        self.eps = 0.5
+        # self.slow_down_distance = 1.0
+        
+        # probably from config file to get cam_skip_frames, distance 
+        self.camera_only = False
+        self.camera_module = Camera_serial(cam_skip_frames=100, distance=100, show_camera=self.camera_only, return_true_output=True)
+
+        self.goal = None
+        
+    def depth_fn(self, d):
+        if d > self.eps:
+            return 1
+        else:
+            return 0
+        
+    def yaw_from_coords(self, xy, depth): # v1 is that it simply turns right or left depending on where the object is. its not very fine tuned.
+        self.pupper_center = 60 # assuming pupper is at center of camera. width of camera: 120
+        x,_=xy
+        if x > self.pupper_center: # on right
+            return 1
+        elif x < self.pupper_center: # on lefr
+            return -1
+        else:
+            return 0 
+
+    def run(self, state, command):
+        if command.follow_event:
+            if self.in_follow_state == False:
+                self.in_follow_state = True
+                print("t pressed, entered follow state")
+            else:
+                self.in_follow_state = False
+                command.stand_event = True
+                super().run(state, command)
+                print("t pressed, exited follow state")
+        if self.in_follow_state:
+            object_center, depth = self.camera_module.get_camera_details() 
+
+            if depth is None and self.goal is None:
+                if state.behavior_state != BehaviorState.REST:
+                    command.stand_event = True
+                    super().run(state, command)
+                    print("pupper standing because no object detected and no goal is set already")
+
+            if object_center is not None and depth is not None:
+                self.goal = (object_center, depth)
+            
+            if self.goal is not None:
+                delta_yaw = self.yaw_from_coords(self.goal[0])
+                how_far = self.depth_fn(self.goal[1])
+                
+                if how_far != 0:  
+                    self.ly_ = self.l_alpha * how_far + (1 - self.l_alpha) * self.ly_         # l_alpha*1 for forward. l_alpha*-1 for backward
+                    x_vel = self.ly_ * self.config.max_x_velocity
+                    y_vel = self.lx_ * -self.config.max_y_velocity
+                    command.horizontal_velocity = np.array([x_vel, y_vel])
+        
+                    self.rx_ = self.r_alpha * delta_yaw + (1 - self.r_alpha) * self.rx_ #r_alpha*1 for right. r_alpha*-1 for left
+                    command.yaw_rate = self.rx_ * self.config.max_yaw_rate
+                    if state.behavior_state != BehaviorState.TROT:
+                        command.trot_event = True
+
+                    super().run(state, command)
+                else:
+                    if state.behavior_state != BehaviorState.REST:
+                        print("goal reached")
+                        command.stand_eveent = True
+                        super().run(state, command)
+        else:
+            super().run(state, command)
+
+
+    # some logic:
+    # 1. so pupper goes in follow mode
+    # 2. it starts detecting using camera
+    # 3. while camera has no object --> it just stands
+    # 4. when camera detects an object and returns depth, (x,y) --> saves depth and (x,y) and starts moving to it
+    # 5. now that it has a goal, keep going towards goal and check camera every x units 
+    #   - this is useful for simulation cuz loading camera takes a lot of compute
+    #   - this may be useful irl cuz camera will be shaking up and down a lot
+    #   - alternatively we can check camera more frequently if goal is near
